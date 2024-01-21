c8a509e1c78194df99cb82baa4a2f64b4d6c3c3d
Volodymyr Khaidarov <kvm5050@gmail.com>
2024-01-21 18:46:58 +0200

    MF-4: removed redundant configuration

--- resource-service/src/main/java/com/epam/microservice/config/ApplicationConfiguration.java
+++ resource-service/src/main/java/com/epam/microservice/config/ApplicationConfiguration.java
@@ -1,16 +1,9 @@
 package com.epam.microservice.config;
 
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -21,9 +14,6 @@ public class ApplicationConfiguration {
   @Value("${aws.service.url}")
   private String awsServiceUrl;
 
-  @Value(value = "${spring.kafka.bootstrap-servers}")
-  private String bootstrapAddress;
-  
   @Bean
   public S3Client getS3Client() {
     return S3Client.builder()
@@ -32,25 +22,5 @@ public class ApplicationConfiguration {
         .credentialsProvider(DefaultCredentialsProvider.create())
         .region(Region.US_EAST_1)
         .build();
-  }
-
-  @Bean
-  public KafkaTemplate<String, String> kafkaTemplate() {
-    return new KafkaTemplate<>(producerFactory());
-  }
-
-  @Bean
-  public ProducerFactory<String, String> producerFactory() {
-    Map<String, Object> configProps = new HashMap<>();
-    configProps.put(
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-            bootstrapAddress);
-    configProps.put(
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-            StringSerializer.class);
-    configProps.put(
-            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-            StringSerializer.class);
-    return new DefaultKafkaProducerFactory<>(configProps);
   }
 }

8452128f9448305d692a0d0308adb93728f961bb
Volodymyr Khaidarov <kvm5050@gmail.com>
2024-02-17 23:31:43 +0200

    MF-6: added unit and component tests

---
+++ resource-service/src/test/java/com/epam/microservice/service/ResourceServiceImplTest.java
@@ -0,0 +1,77 @@
+package com.epam.microservice.service;
+
+import com.epam.microservice.model.Resource;
+import com.epam.microservice.repository.ResourceRepository;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.mock.web.MockMultipartFile;
+
+class ResourceServiceImplTest {
+
+  private static final Integer RESOURCE_ID_1 = 1;
+  private static final Integer RESOURCE_ID_2 = 2;
+  private static final String S3BUCKET = "s3bucket";
+  private static final String FILENAME = "AudioTrack.mp3";
+  private static final byte[] PAYLOAD = "DoReMi".getBytes();
+
+  @Mock
+  private ResourceRepository resourceRepository;
+  @Mock
+  private KafkaTemplate<String, String> kafkaTemplate;
+  @Mock
+  private S3Service s3Service;
+
+  @Test
+  void addResourceTest() {
+
+    MockitoAnnotations.openMocks(this);
+    ResourceService resourceService = new ResourceServiceImpl(resourceRepository, s3Service, kafkaTemplate);
+    MockMultipartFile multipartFile = new MockMultipartFile(FILENAME, FILENAME, null, PAYLOAD);
+    Resource resource = new Resource(RESOURCE_ID_1, S3BUCKET, FILENAME);
+
+    Mockito.when(s3Service.getS3Bucket()).thenReturn(S3BUCKET);
+    Mockito.when(s3Service.uploadResource(multipartFile)).thenReturn(FILENAME);
+    Mockito.when(resourceRepository.findByResourceKey(FILENAME)).thenReturn(List.of());
+    Mockito.when(resourceRepository.save(Mockito.any())).thenReturn(resource);
+
+    Integer resourceId = resourceService.addResource(multipartFile);
+
+    Assertions.assertEquals(RESOURCE_ID_1, resourceId);
+  }
+
+  @Test
+  void getResourceTest() {
+
+    MockitoAnnotations.openMocks(this);
+    ResourceService resourceService = new ResourceServiceImpl(resourceRepository, s3Service, kafkaTemplate);
+    Resource resource = new Resource(RESOURCE_ID_1, S3BUCKET, FILENAME);
+
+    Mockito.when(resourceRepository.findById(RESOURCE_ID_1)).thenReturn(Optional.of(resource));
+    Mockito.when(s3Service.downloadResource(FILENAME)).thenReturn(Optional.of(PAYLOAD));
+
+    byte[] payload = resourceService.getResourceById(RESOURCE_ID_1);
+
+    Assertions.assertEquals(PAYLOAD, payload);
+  }
+
+  @Test
+  void deleteResourcesTest() {
+
+    MockitoAnnotations.openMocks(this);
+    ResourceService resourceService = new ResourceServiceImpl(resourceRepository, s3Service, kafkaTemplate);
+    Resource resource = new Resource(RESOURCE_ID_1, S3BUCKET, FILENAME);
+
+    Mockito.when(resourceRepository.findById(RESOURCE_ID_1)).thenReturn(Optional.of(resource));
+    Mockito.when(resourceRepository.findById(RESOURCE_ID_2)).thenReturn(Optional.empty());
+
+    List<Integer> deletedIds = resourceService.deleteResources(RESOURCE_ID_1 + "," + RESOURCE_ID_2);
+
+    Assertions.assertEquals(List.of(RESOURCE_ID_1), deletedIds);
+  }
+}
---
+++ song-service/src/test/java/com/epam/microservice/SongServiceComponentTest.java
@@ -0,0 +1,39 @@
+package com.epam.microservice;
+
+import com.epam.microservice.model.SongMetadata;
+import com.epam.microservice.repository.SongRepository;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SongServiceComponentTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private SongRepository songRepository;
+
+    @Test
+    void getSongMetadata() throws Exception {
+        SongMetadata songMetadata = new SongMetadata();
+        songMetadata.setName("AudioTrack");
+        songMetadata.setResourceId("666");
+        songRepository.save(songMetadata);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/songs/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("1")))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("AudioTrack")))
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("666")));
+    }
+}

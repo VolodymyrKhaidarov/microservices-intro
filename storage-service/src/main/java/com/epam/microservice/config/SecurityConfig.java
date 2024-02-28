36e7ace467d01e224aa074d419951f0b8f469185
Volodymyr Khaidarov <kvm5050@gmail.com>
2024-02-28 22:40:48 +0200

    MF-16: configured Keycloak security

--- storage-service/src/main/java/com/epam/microservice/config/SecurityConfig.java
+++ storage-service/src/main/java/com/epam/microservice/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.epam.microservice.config;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,8 +18,32 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-    return http.build();
+    return http.authorizeHttpRequests(
+            authorize ->
+                authorize
+                    .requestMatchers(HttpMethod.GET, "/storages").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/storages").hasRole("admin")
+                    .requestMatchers(HttpMethod.DELETE, "/storages").hasRole("admin"))
+        .oauth2ResourceServer(
+            oauth2 ->
+                oauth2.jwt(
+                    jwtConfigurer ->
+                        jwtConfigurer.jwtAuthenticationConverter(jwtAuthenticationConverter())))
+        .build();
+  }
+
+  @Bean
+  public JwtAuthenticationConverter jwtAuthenticationConverter() {
+    JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+    converter.setJwtGrantedAuthoritiesConverter(
+        jwt ->
+            Optional.ofNullable(jwt.getClaimAsMap("realm_access"))
+                .map(claim -> (List<String>) claim.get("roles"))
+                .orElse(List.of())
+                .stream()
+                .map(roleName -> "ROLE_" + roleName)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList()));
+    return converter;
   }
 }
